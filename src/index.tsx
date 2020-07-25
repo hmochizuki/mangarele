64c7e8b5455544f323035d10084bab9d9e2481ec
H_Mochizuki <h_mochizuki@mochidzukikoushinoMacBook-Air.local>
2020-07-26 01:10:19 +0900

    feat: implemets views

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import firebase from "firebase/app";
+import App from "./App";
+
+import FirebaseApp from "./FirebaseApp";
+import { ThemeContext } from "./contexts";
+import mangarelTheme from "./theme";
+import firebaseConfig from "./firebase-config";
+import * as serviceWorker from "./serviceWorker";
+
+import "semantic-ui-css/semantic.min.css";
+import "./index.css";
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <FirebaseApp>
+      <ThemeContext.Provider value={mangarelTheme}>
+        <App />
+      </ThemeContext.Provider>
+    </FirebaseApp>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
